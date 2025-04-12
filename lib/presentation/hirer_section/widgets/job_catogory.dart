92e3e051f04c7e46a7c6d22db651b405d71cc9dc
Abhishek <abhishekramesh2424@gmail.com>
2025-04-13 00:40:56 +0530

    fix(profile): correct image upload bug on profile page

--- lib/presentation/hirer_section/widgets/job_catogory.dart
+++ lib/presentation/hirer_section/widgets/job_catogory.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hey_work/presentation/hirer_section/job_detail_page.dart/job_detail.dart';
-import 'package:intl/intl.dart';
+
 
 
 class JobCategoriesSection extends StatelessWidget {
